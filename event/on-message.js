bc65ea2e6ddfe754c5d1401449432c546e975dff
凹凸妹寻 <bobpan@users.noreply.github.com>
2021-02-28 15:26:15 +0800

    增加微信群搜索能力

--- event/on-message.js
+++ event/on-message.js
@@ -14,53 +14,60 @@ const {
                   + '<' + (sender && sender.name()) + '>'
                   + ':' + message,
       )
-  
-      //if (message.self() || room) {
-      //  console.info('message is sent from myself, or inside a room.')
-      //  return
-      //}
-  
-      /********************************************
-       *
-       * 从下面开始修改vvvvvvvvvvvv
-       *
-       */
-      if (!sender) return
-  
-      if (/^bobot/i.test(content)){
-        await message.say('欢迎你来[鼓掌]')
-  
-        const bobotR = await this.Room.find({ topic: 'bobot' })
-        if (!bobotR) return
-  
-        if (await bobotR.has(sender)) {
-          await sender.say('你已经在bobot微信群里面啦')
+        
+      if (message.self()) {
+        if (/^exit/i.test(content)) {
+          await sender.say('请求退出')
+          process.exit()
+        } else return
+      } else {
+        if (!sender) return
+        // message is inside a room.
+        if (room ) return
+        // Message discarded because its TOO OLD(than 1 minute)
+        if (message.age() > 60) return
+        //非Message#Text类型
+        if(message.type() != this.Message.Type.Text) return
+        /********************************************
+         *
+         * 默认加群动作
+         *
+         */
+
+        await message.say('查找微信群：' + content)
+        const bobotR = await this.Room.find({ topic: new RegExp(content, 'i') })
+        console.log(content,bobotR)
+        if (bobotR){
+          const topic = await bobotR.topic()
+          console.log(topic)
+          await message.say('找到微信群：' + topic)
+          if (await bobotR.has(sender)) {
+            await sender.say('你已经在bobot微信群里面啦')
+          } else {
+            await sender.say('已将你拉进微信群啦')
+            await bobotR.add(sender)
+          }
+          return
+        } else {
+          await sender.say('没有找到这个群，如果是您的群希望被人搜到，拉我进群就可以啦')
+        } 
+        
+        /**
+         *
+         * 加群结束
+         *
+         */
+        /*********************************************/
+
+        if (/^bpan1/i.test(content)) {
+          await sender.say('对对对，我就是沃尔玛那枚小小的产品经理')
+          return
+        } else if (/^音乐/i.test(content)) {
+          const fileBox = FileBox.fromUrl('https://7465-test-666666-1257636227.tcb.qcloud.la/jay%20zhou.jpeg?sign=94ead5a2ea2bffe9a75cb91dec259bd0&t=1605359493')
+          await sender.say(fileBox)
           return
         }
-  
-        await sender.say('已将你拉进bobot微信群啦')
-        await bobotR.add(sender)
-        return
-  
-      } else if (/^bpan1/i.test(content)) {
-        await sender.say('对对对，我就是沃尔玛那枚小小的产品经理')
-        return
-      } else if (/^音乐/i.test(content)) {
-        const fileBox = FileBox.fromUrl('https://7465-test-666666-1257636227.tcb.qcloud.la/jay%20zhou.jpeg?sign=94ead5a2ea2bffe9a75cb91dec259bd0&t=1605359493')
-        await sender.say(fileBox)
-        return
-      } 
-    
-      if (message.self() && /^exit/i.test(content)) {
-        await sender.say('***请求退出成功***')
-        process.exit()
       }
-      /**
-       *
-       * 到这里结束修改^^^^^^^^^^^^
-       *
-       */
-      /*********************************************/
     } catch (e) {
       console.error(e)
     }
