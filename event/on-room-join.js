bc619e1b0a12a41dafb85c597108be71d41b855f
凹凸妹寻 <bobpan@users.noreply.github.com>
2021-02-28 15:33:45 +0800

    回滚

--- event/on-room-join.js
+++ event/on-room-join.js
@@ -1,59 +1,58 @@
 const {
-    Friendship,
+    Contact,
+    Room,
     Wechaty,
-    // Room,
-  }                 = require('wechaty')
+  }             = require('wechaty')
   
-  async function onFriendship (
-    request,
+  async function onRoomJoin (
+    room,
+    inviteeList,
+    inviter,
   ) {
     try {
-      const contact = request.contact()
-  
-      if (request.type() === Friendship.Type.Confirm) {
-        console.info('New friend ' + contact.name() + ' relationship confirmed!')
-        return
-      }
-  
+      const inviteeName = inviteeList.map(c => c.name()).join(', ')
       /********************************************
        *
        * 从这里开始修改 vvvvvvvvvvvv
        *
        */
-      await request.accept()
   
-      setTimeout(
-        async _ => {
-          await contact.say('你好，' + contact.name() + '，我可以提供微信群搜索服务，输入关键字并可快速查找和加群，快试试吧（例：bobot）')
-          await contact.say('如果您也有群希望被人搜到，只需要拉我进群就行啦')
-        },
-        3000,
-      )
-  
-      if (request.hello() === 'bobot') {
-        const myRoom = await this.Room.find({ topic: 'bobot' })
-        if (!myRoom) return
-        setTimeout(
-          async _ => {
-            await myRoom.add(contact)
-            await myRoom.say('welcome ' + contact.name())
-          },
-          3000,
+      if (await room.topic() !== 'bobot') {
+        await this.say('Room ' + await room.topic()
+                        + ' got new memeber ' + inviteeName
+                        + ' invited by ' + inviter.name(),
         )
+        return
       }
+  
+      const inviterIsMyself = inviter.self()
+  
+      if (inviterIsMyself) {
+        await room.say('Welcome to my room: ' + inviteeName)
+        return
+      }
+  
+      await room.say('请勿私自拉人。需要拉人请加我', inviter)
+      await room.say('请先加我好友并留言bobot，将自动拉你入群。先把你移出啦。', inviteeList)
+  
+      inviteeList.forEach(async c => {
+        await room.del(c)
+      })
   
       /**
        *
-       * 到这里结束修改 ^^^^^^^^^^^^
+       * 到这里结束修改^^^^^^^^^^^^
        *
        */
-      /*******************************************/
+      /*********************************************/
+  
     } catch (e) {
       console.info(e)
     }
+  
   }
   
   module.exports = {
-    default: onFriendship,
-    onFriendship,
+    default: onRoomJoin,
+    onRoomJoin,
   }
